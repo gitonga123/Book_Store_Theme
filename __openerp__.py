5ae0dc61d84f16cf1cee249a4e098af71fe6b1cc
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-24 15:40:11 +0300

    Book Store Theme

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+{
+    # Theme information
+    'name': "Book Theme",
+    'description': """
+        This theme extends the Book Store features
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "Daniel Mutwiri",
+    'website': "www.youtube.com",
+}
